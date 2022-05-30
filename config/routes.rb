c446cdc822faa0558a496899fef68c7903e435e3
Gegardus <stgegard@gmail.com>
2022-05-31 00:06:31 +0400

    Add model, migration, controller, view and roots

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'users#index'
+  get 'users', to: 'users#index'
+  get 'users/:user_id', to: 'users#show'
+  get 'users/:user_id/foods', to: 'foods#index'
+  get 'users/:user_id/foods/:id', to: 'foods#show'
+  get 'users/:user_id/recipes', to: 'recipes#index'
+  get 'users/:user_id/recipes/:id', to: 'recipes#show'
+  get 'recipe_foods', to: 'recipe_foods#index'
+  get 'recipe_foods/:id', to: 'recipe_foods#show'
 end
