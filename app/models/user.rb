a59bc019ff676c7adc1b1e9d224f95f4f0c0610a
Gegardus <stgegard@gmail.com>
2022-06-01 01:15:53 +0400

    Add food-list

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
+  has_many :foods
+  has_many :recipes
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true, length: { in: 1..50 }
+
+  def admin?(requested_role)
+    role == requested_role.to_s
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,23 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'users#index'
-  get 'users', to: 'users#index'
-  get 'users/:user_id', to: 'users#show'
-  get 'users/:user_id/foods', to: 'foods#index'
-  get 'users/:user_id/foods/:id', to: 'foods#show'
-  get 'users/:user_id/recipes', to: 'recipes#index'
-  get 'users/:user_id/recipes/:id', to: 'recipes#show'
-  get 'recipe_foods', to: 'recipe_foods#index'
-  get 'recipe_foods/:id', to: 'recipe_foods#show'
+
+  resources :foods, only: [:index, :new, :create, :destroy]
+
+  resources :recipes, only: [:index, :show, :new, :create, :destroy, :update] do
+    resources :recipe_foods, only: [:new, :create, :destroy, :edit, :update]
+  end
+  
+  resources :public_recipes, only: [:index]
+
+  resources :shopping_list, only: [:index]
+
+  # get 'users', to: 'users#index'
+  # get 'users/:user_id', to: 'users#show'
+  # get 'users/:user_id/foods', to: 'foods#index'
+  # get 'users/:user_id/foods/:id', to: 'foods#show'
+  # get 'users/:user_id/recipes', to: 'recipes#index'
+  # get 'users/:user_id/recipes/:id', to: 'recipes#show'
+  # get 'recipe_foods', to: 'recipe_foods#index'
+  # get 'recipe_foods/:id', to: 'recipe_foods#show'
 end
