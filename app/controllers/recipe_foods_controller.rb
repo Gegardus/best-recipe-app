58a3c1056110cf9eff4d0c91a66db033085af492
farouqdaif1 <daiffarouq@gamil.com>
2022-06-01 15:33:23 +0200

    Add recipe list and details and puplic recipe

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -1,5 +1,9 @@
 class RecipeFoodsController < ApplicationController
-  def index; end
+  def index
+    @puplic_recipe = Recipe.where(public: true).includes(:user)
+  end
 
-  def show; end
+  def show
+    @pupli_recipe_details = Recipe.find_by_id(params[:id])
+  end
 end
