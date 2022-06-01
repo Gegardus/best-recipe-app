5c131f046deab6775a244c4b1efd9b6828b2950e
Gegardus <stgegard@gmail.com>
2022-06-01 19:08:47 +0400

    Add navbar

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,10 @@ Rails.application.routes.draw do
 
   resources :shopping_list, only: [:index]
 
+  devise_scope :user do match "sign_out" => "sessions#destroy", via: [:delete] end 
+
+
+
   # get 'users', to: 'users#index'
   # get 'users/:user_id', to: 'users#show'
   # get 'users/:user_id/foods', to: 'foods#index'
