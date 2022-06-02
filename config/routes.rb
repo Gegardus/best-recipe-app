0b7c0c6c0181f8f1248b43ba3e15129dc00d2d56
Gegardus <stgegard@gmail.com>
2022-06-02 08:14:13 +0400

    Refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,6 @@
-# Rails.application.routes.draw do
-#   devise_for :users
-#   root 'users#index'
-
-# resources :shopping_list, only: [:index]
-# resources :foods, only: [:index,:show, :new, :create, :destroy]
-#   resources :recipe_public, only:[:index,:show]
-#   resources :users ,only: [:index,:show] do 
-#     resources :recipes, only: [:index,:show,:destroy]
-#   end
-# end
 
 Rails.application.routes.draw do
   devise_for :users
-  root 'users#index'
-
   resources :foods, only: [:index, :new, :create, :destroy]
 
   resources :recipes, only: [:index, :show, :new, :create, :destroy, :update] do
@@ -23,13 +10,6 @@ Rails.application.routes.draw do
   resources :public_recipes, only: [:index]
 
   resources :shopping_list, only: [:index]
-
-  devise_scope :user do match "sign_out" => "sessions#destroy", via: [:delete] end 
-
-  resources :recipes, only: [:index, :show, :new, :create, :destroy, :update] do
-    resources :recipe_foods, only: [:new, :create, :destroy, :edit, :update]
-  end
-  
-  resources :public_recipes, only: [:index]
-
+ 
+  root "home_page#index"
 end
