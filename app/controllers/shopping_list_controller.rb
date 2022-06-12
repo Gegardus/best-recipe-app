8591326caa8f70edb66f2913e37ffa11c2d229a9
Gegardus <stgegard@gmail.com>
2022-06-12 14:29:42 +0400

    Refactor styling

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -1,17 +1,25 @@
+# frozen_string_literal: true
+
 class ShoppingListController < ApplicationController
   def index
-    @recipes = Recipe.all
-    @ingredients = RecipeFood.where(recipe_id: @recipes.map(&:id))
-    @foods = Food.where(id: @ingredients.map(&:food_id))
-  end
+    @food_amount = 0
 
-  def total_price
-    price = 0
-    @ingredients.each do |recipe_food|
-      price += Food.find(recipe_food.food_id).price * recipe_food.quantity
+    current_user.recipes.each do |recipe|
+      @food_amount += recipe.recipe_foods.count
     end
-    price
-  end
 
-  helper_method :total_price
+    @total_price = 0
+
+    current_user.recipes.each do |recipe|
+      recipe.recipe_foods.each do |recipe_food|
+        @total_price += recipe_food.food.price * recipe_food.quantity
+      end
+    end
+
+    @recipe_foods = []
+
+    current_user.recipes.each do |recipe|
+      @recipe_foods.push(recipe.recipe_foods)
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
-  get 'recipe_/public'
   devise_for :users
-  root 'users#show'
+  # route for home page
+  root 'users#index'
 
-resources :shopping_list, only: [:index]
-  resources :users ,only: [:show] do 
-    resources :recipe_public,only:[:index,:show]
-    resources :recipes, only: [:index,:show,:destroy,:new ,:create] do
-      resources :recipe_food,only:[:destroy,:new ,:create]
-    end 
-    resources :foods, only: [:index,:show, :new, :create, :destroy]
+  resources :foods, only: [:index, :new, :create, :destroy]
+
+  resources :recipes, only: [:index, :show, :new, :create, :destroy, :update] do
+    resources :recipe_foods, only: [:new, :create, :destroy, :edit, :update]
   end
-end
+  
+  resources :public_recipes, only: [:index]
 
+  resources :shopping_list, only: [:index]
+end
